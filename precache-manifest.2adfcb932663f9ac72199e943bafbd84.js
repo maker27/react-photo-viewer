03d7ab60f85a906e0a09dfd5d75e3518c1852172
maker27 <evgeniy@maker27.ru>
2020-02-25 22:08:44 +0300

    Updates

---
+++ precache-manifest.2adfcb932663f9ac72199e943bafbd84.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a9ba69afbcee612e48e0469d895be43",
+    "url": "/react-photo-viewer/index.html"
+  },
+  {
+    "revision": "dc2619f05d7e9538695c",
+    "url": "/react-photo-viewer/static/css/main.2f12ea30.chunk.css"
+  },
+  {
+    "revision": "7bca30aea5d5bfcc3d6c",
+    "url": "/react-photo-viewer/static/js/2.270609bb.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/react-photo-viewer/static/js/2.270609bb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dc2619f05d7e9538695c",
+    "url": "/react-photo-viewer/static/js/main.c2d59a15.chunk.js"
+  },
+  {
+    "revision": "1203ca3611c3003f66c8",
+    "url": "/react-photo-viewer/static/js/runtime-main.1f5ceb74.js"
+  }
+]);
